61eb5a19f3e5eb67578108bc66550d1f91c65624
Zebulun Arendsee <arendsee@iastate.edu>
2020-04-12 00:09:26 -0500

    Add fold test

--- test-core.cpp
+++ test-core.cpp
@@ -36,10 +36,16 @@ int main(int argc, char * argv[]){
 
     std::vector<int> zs = morloc_zipWith(add, xs, xs);
 
+    double b = 0.0;
+    std::vector<double> ws{12.1,23.2,134.3};
+    double summed = morloc_fold(morloc_add, b, ws);
+
     std::cout << ys[0] << " " << ys[1] << " " << ys[2] << std::endl; 
     std::cout << ss[0] << " " << ss[1] << " " << ss[2] << std::endl;
 
     std::cout << zs[0] << " " << zs[1] << " " << zs[2] << std::endl; 
+
+    std::cout << "sum(12.1,23.2,134.3)=" << summed << std::endl;
 
     std::vector<std::string> bases{"asdf", "qwer", "wert"};
     std::vector<std::string> names = morloc_enumerateWith(makeName, bases);
