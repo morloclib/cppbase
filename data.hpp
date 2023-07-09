d0aad8703e2b19927a4f1e49aa64e269baa71f25
Zebulun Arendsee <zbwrnz@gmail.com>
2023-07-09 16:50:41 -0500

    Add Unit packers

--- data.hpp
+++ data.hpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <utility>
 #include <cassert>
+#include <mlc.hpp>
 
 template <class A, class B>
 std::map<A,B> morloc_packMap(std::tuple<std::vector<A>,std::vector<B>> items){
@@ -27,6 +28,16 @@ std::tuple<std::vector<A>,std::vector<B>> morloc_unpackMap(std::map<A,B> m){
         b.push_back(std::get<1>(tuple));
     }
     return std::make_tuple(a, b);
+}
+
+// packUnit Cpp :: pack => "mlc::Unit" -> "int"
+int morloc_packUnit(mlc::Unit u){
+    return 0;
+}
+
+// unpackUnit Cpp :: unpack => "int" -> "mlc::Unit"
+mlc::Unit morloc_unpackUnit(int x){
+    return mlc::Unit();
 }
 
 #endif
